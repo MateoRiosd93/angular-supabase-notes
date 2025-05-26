e73419a47618ffff8c522ef34cdd1d8b524d157b
marios <marios@bancolombia.com.co>
2025-05-26 09:55:06 -0500

    feature(config): Se realiza configuracion de environments y rutas.

---
+++ src/app/auth/features/auth-log-in/auth-log-in.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-log-in',
+  imports: [],
+  template: `
+    <p>
+      auth-log-in works!
+    </p>
+  `,
+  styleUrl: './auth-log-in.component.css'
+})
+export default class AuthLogInComponent {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    SUPABASE_URL: 'https://wglwwaljbbizdzrvjtbz.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnbHd3YWxqYmJpemR6cnZqdGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMzI5NDMsImV4cCI6MjA2MzcwODk0M30.Ctb-UIdN00hDKnHCtVz7WomWOzJbx3ELBxk3avDt9_U'
+};
