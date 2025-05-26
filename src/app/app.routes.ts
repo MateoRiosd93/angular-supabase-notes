6f1aa25eceec5ad523dc23ea04a75aeda47cbe0a
marios <marios@bancolombia.com.co>
2025-05-26 18:02:00 -0500

    feature(route): Se agrega ruta por defecto para el home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => import('./auth/features/auth-shell/auth-routing')
+    },
+    {
+        path: '**',
+        redirectTo: 'auth'
     }
 ];
